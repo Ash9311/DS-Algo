5eb5cb19c7c1a284219d6b3e0c99792890107457
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2020-10-16 16:45:42 +0530

    Update stackLL.c

--- stackLL.c
+++ stackLL.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 struct Node{
     char data;
@@ -65,15 +66,61 @@ int isBalanced(char *exp){
     else {
         return 0;
     }
-    
 }
 
+int pre(char x){
+    if(x=='+' || x=='-'){
+        return 1;
+    }
+    else if(x=='*'||x=='/'){
+        return 2;
+    }
+    return 0;
+}
+
+int isOperand(char x){
+    if(x=='+'||x=='-'||x=='*'||x=='/'){
+        return 0;
+    }
+    else{
+        return 1;
+    }
+}
+
+char *InToPost(char *infix){
+    int i=0,j=0;
+    char *postfix;
+    int len=strlen(infix);
+    postfix=(char *)malloc((len+2)*sizeof(char));
+    
+    while(infix[i]!='\0'){
+        if(isOperand(infix[i])){
+            postfix[j++]=infix[i++];
+        }
+        else{
+            if(pre(infix[i])>pre(top->data)){
+                push(infix[i++]);
+            }
+            else{
+                postfix[j++]=pop();
+            }
+        }
+    }
+    while(top!=NULL){
+        postfix[j++]=pop();
+    }
+    postfix[j]='\0';
+    return postfix;
+}
 
 int main(){
-   char *exp="((a+b)*(c-d))"; //pointer is used so that it can be taken as a                                                                 string
-    
-    
-    printf("%d ",isBalanced(exp));
+  // char *exp="((a+b)*(c-d))"; //pointer is used so that it can be taken as a  string                                                      
+     char *infix="a+b*c-d/e";
+     push('#');
+     char *postfix=InToPost(infix);
+     printf("%s ",postfix);
+
+      return 0;
 }
 
 
